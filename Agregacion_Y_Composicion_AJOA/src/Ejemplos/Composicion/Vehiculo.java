01252b7e4699ec817cc0f4957b87bf1a89166595
Ale64476 <148830719+Ale64476@users.noreply.github.com>
2023-11-21 12:19:24 -0600

    clase padre

--- Agregacion_Y_Composicion_AJOA/src/Ejemplos/Composicion/Vehiculo.java
+++ Agregacion_Y_Composicion_AJOA/src/Ejemplos/Composicion/Vehiculo.java
@@ -1,5 +1,22 @@
 package Ejemplos.Composicion;
 
-public class Vehiculo {
+public abstract class Vehiculo {
+	String marca;
+	String numDeRuedas;
 	
+	public abstract void avanzar();
+	public abstract void frenar();
+	
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public void setNumDeRuedas(String ruedas) {
+		this.numDeRuedas = ruedas;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public String getNumDeRuedas() {
+		return numDeRuedas;
+	}
 }
