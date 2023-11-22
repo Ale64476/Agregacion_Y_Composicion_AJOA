de03e73b188b59e093cfc358d3dda3a698e5dd3f
Ale64476 <148830719+Ale64476@users.noreply.github.com>
2023-11-21 20:23:31 -0600

    controladora y fin de la tarea

--- Agregacion_Y_Composicion_AJOA/src/Ejemplos/Agregacion/Juego.java
+++ Agregacion_Y_Composicion_AJOA/src/Ejemplos/Agregacion/Juego.java
@@ -5,7 +5,27 @@ public class Juego {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Mago mago = new Mago();
+		Bruja bruja = new Bruja();
+		Caballero caballero = new Caballero();
+		Campesino campesino = new Campesino();
+		Guerrero guerrero = new Guerrero();
+		GuerreroAcorazado gAcorazado = new GuerreroAcorazado();
+		MagoOscuro magoOscuro = new MagoOscuro();
+		Ninja ninja = new Ninja();
+		Paladin paladin = new Paladin();
+		Sanadora sanadora = new Sanadora();
 		
+		System.out.println("Oh no! Estan atacando nuestra aldea! Mira! Los habitantes se defienden!");
+		mago.atacar();
+		bruja.atacar();
+		caballero.atacar();
+		campesino.atacar();
+		guerrero.atacar();
+		gAcorazado.atacar();
+		magoOscuro.atacar();
+		ninja.atacar();
+		paladin.atacar();
+		sanadora.atacar();
 	}
 
 }
